e95c1dd22332eb1eb0629f47e1e59fe6b50ca654
Dreamwalker <aristojeff@gmail.com>
2020-07-12 13:18:04 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import ClickMeApp from './App4';
-import App from './App';
-import ButtonBasics from './App5';
-import App7 from './App7';
+import App9 from './App9';
 
-AppRegistry.registerComponent(appName, () => App7);
+AppRegistry.registerComponent(appName, () => App9);
